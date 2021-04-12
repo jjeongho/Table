f3d90e2ca530955dc89c0ad6996e3a2cda483a28
Hwang Jeong Ho <jakchung1021@gmail.com>
2021-04-12 16:19:08 +0900

    TableView 생성 연습!

--- Table/ViewController.swift
+++ Table/ViewController.swift
@@ -7,11 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+    
+    
+    @IBOutlet weak var TableViewMain: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //Data의 개수
+    return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //어떠한 데이터
+        let cell = UITableViewCell.init(style:.default, reuseIdentifier: "TableCellType1")
+        
+        cell.textLabel?.text = "\(indexPath.row)"
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        TableViewMain.delegate = self
+        TableViewMain.dataSource = self
     }
 
 
